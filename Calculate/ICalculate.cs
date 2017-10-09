95489cda256f216a65dee839e1dcb5fdd5c48791
Jethro Badenhorst <badenhorstj@conlog.co.za>
2017-10-09 11:02:55 +0200

    Revert "Revert "Add project files.""
    This reverts commit 172eb3330a170e26cc23660b20a950d1cc56698f.

---
+++ Calculate/ICalculate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public interface ICalculate
+    {
+        int CalculateResult(int num1, int num2);
+    }
+}
